46749d6e2adf4de7639a2290bf92f32ea4c79cf2
rlemon <rob.lemon@gmail.com>
2016-08-22 11:59:14 -0400

    HANDSSSS

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function getColorWeight(clr1, clr2) {
-	return 	Math.sqrt(Math.pow((clr2.r - clr1.r), 2) + 
+	// return 	Math.sqrt(Math.pow((clr2.r - clr1.r), 2) + 
+	// 		Math.pow((clr2.g - clr1.g), 2) + 
+	// 		Math.pow((clr2.b - clr1.b), 2)) // because I'm not checking an actual distance, just weight, fuck sqrt
+	return 	Math.pow((clr2.r - clr1.r), 2) + 
 			Math.pow((clr2.g - clr1.g), 2) + 
-			Math.pow((clr2.b - clr1.b), 2))
+			Math.pow((clr2.b - clr1.b), 2)
 }
 
 function getClosestColor(clr) {
@@ -48,8 +51,9 @@ image.crossOrigin = 'Anonymous';
 image.onload = init;
 image.src = 'http://i.imgur.com/DbCf4Ab.jpg';
 
-//const resolution = 4; // ~3 seconds
-const resolution = 2; // ~13 seconds, I'd love to trim this to < 5 if possible :/ 
+const resolution = 4; // ~3 seconds
+//const resolution = 2; // ~13 seconds, I'd love to trim this to < 5 if possible :/ 
+const grid = [];
 
 function init() {
 	console.time('generate');
@@ -57,19 +61,32 @@ function init() {
 	canvas.width = this.width;
 	context.drawImage(this, 0, 0);
 	const idata = context.getImageData(0, 0, this.width, this.height);
-	const grid = [];
-	for( let y = 0; y < this.height; y += resolution ) {
+	for( let y = 0; y < this.height - resolution; y += resolution ) {
 		const yIndex = y/resolution;
 		grid[yIndex] = [];
-		for( let x = 0; x < this.width; x += resolution ) {
+		for( let x = 0; x < this.width - resolution; x += resolution ) {
 			const xIndex = x / resolution;
 			const o = x * 4 + y * 4 * this.width;
-			const rgb = getColor(idata, x, y, resolution, resolution);
+			const rgb = getColor(idata, x + resolution, y + resolution, resolution, resolution);
 			const closest = getClosestColor(rgb);
 			const lum = idata.data[o] * 0.2126 + idata.data[o+1] * 0.7152 + idata.data[o+2] * 0.0722;
-			grid[yIndex][xIndex] = {rgb: closest, lum};
+			grid[yIndex][xIndex] = {dnc: closest, lum};
 		}
 	}
 	console.timeEnd('generate');
-	document.body.appendChild(this);
+	render();
+}
+
+function render() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	for( let y = 0; y < grid.length; y++ ) {
+		for( let x = 0; x < grid[y].length; x++ ) {
+			const lum = grid[y][x].lum;
+			const dnc = grid[y][x].dnc.code;
+			context.beginPath();
+			context.fillStyle = `rgb(${dnc.r},${dnc.g},${dnc.b})`;
+			context.fillRect(x * resolution, y * resolution, resolution, resolution);
+			context.closePath();
+		}
+	}
 }
