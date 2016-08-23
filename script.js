128b6862a6e6f9a2aa6a8bd9271612820ffc02b9
rlemon <rob.lemon@gmail.com>
2016-08-23 14:09:47 -0400

    ooooohhh oooooo ooooooooohhhohohohohoh

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const resolution = 4; // ~3 seconds
 //const resolution = 2; // ~13 seconds, I'd love to trim this to < 5 if possible :/ 
 
 function main() {
-	console.time('generate');
+	console.time('generate new matrix');
 	canvas.height = this.height;
 	canvas.width = this.width;
 	context.drawImage(this, 0, 0);
@@ -69,23 +69,55 @@ function main() {
 			const o = x * 4 + y * 4 * this.width;
 			const rgb = getColor(idata, x + resolution, y + resolution, resolution, resolution);
 			const closest = getClosestColor(rgb);
-			const lum = idata.data[o] * 0.2126 + idata.data[o+1] * 0.7152 + idata.data[o+2] * 0.0722;
+			const trgb = closest.code;
+			const lum = trgb.r * 0.2126 + trgb.g * 0.7152 + trgb.b * 0.0722;
 			grid[yIndex][xIndex] = {dnc: closest, lum};
 		}
 	}
-	console.timeEnd('generate');
+	console.timeEnd('generate new matrix');
+	// 
+	console.time('render full image')
+	let cX = 0;
+	let cY = 0;
+	let pY = 0;
+	let pX = 0;
+	const pScale = 2;
+	const scale = 10;
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.height *= scale;
+	canvas.width *= scale;
+	canvas.width += (pScale * (canvas.width / resolution / 10));
+	canvas.height += (pScale * (canvas.height / resolution / 10));
+	context.font = '7pt courier';
+	context.lineWidth = 1;
+	context.fillStyle = '#fff';
+	context.fillRect(0, 0, canvas.width, canvas.height);
 	for( let y = 0; y < grid.length; y++ ) {
+		pX = 0;
+		if( (y+1) % 10 === 0 ) {
+			pY += pScale;
+		}
+		cY = (y) * resolution;
 		for( let x = 0; x < grid[y].length; x++ ) {
 			const lum = grid[y][x].lum;
-			const dnc = grid[y][x].dnc.code;
+			const {code,key} = grid[y][x].dnc;
+			if( (x+1) % 10 === 0 ) {
+				pX += pScale;
+			}
+			cX = (x) * resolution;
 			context.beginPath();
-			context.fillStyle = `rgb(${dnc.r},${dnc.g},${dnc.b})`;
-			context.fillRect(x * resolution, y * resolution, resolution, resolution);
+			context.fillStyle = `rgb(${code.r},${code.g},${code.b})`;
+			context.rect(cX*scale+pX, cY*scale+pY, resolution*scale, resolution*scale);
+			context.strokeStyle = 'rgba(0, 0, 0, 1)';
+			context.fill();
+			context.stroke();
+			context.fillStyle = lum > 100 ? '#000' : '#fff';
+			context.fillText(key, cX*scale+4+pX, cY*scale+(resolution*scale)/2+pY);
 			context.closePath();
 		}
 	}
-	split();
+	console.timeEnd('render full image');
+	//split();
 }
 
 function split() {
@@ -93,14 +125,15 @@ function split() {
 	const h = resolution * 10 * 5;
 	let cX = 0;
 	let cY = 0;
+	const scale = 10;
 	while( cY < canvas.height ) {
 		cX = 0;
 		while( cX < canvas.width )  {
 			const img = document.createElement('canvas');
 			const ctx = img.getContext('2d');
-			img.height = h;
-			img.width = w;
-			ctx.drawImage(canvas, cX, cY, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
+			img.height = h * scale;
+			img.width = w * scale;
+			ctx.drawImage(canvas, cX, cY, canvas.width, canvas.height, 0, 0, canvas.width * scale, canvas.height * scale);
 			document.body.appendChild(img);
 			cX += w;
 		}
